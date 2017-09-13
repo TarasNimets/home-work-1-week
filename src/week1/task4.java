d54c5fc4a3adeac9e52676e9efc600b02f5acbff
TarasNimets <tar.nimets@gmail.com>
2017-09-14 00:46:28 +0300

    added task4

---
+++ src/week1/task4.java
@@ -0,0 +1,25 @@
+package week1;
+
+import java.util.Scanner;
+
+public class task4 {
+	
+	public static void main(String[] arg) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter fractional number -  ");
+		double number = scanner.nextDouble();
+		scanner.close();
+		
+		if (number >= 0 && number <= 1) {
+			System.out.println("Number in range with 0 to 1");
+		}else {
+			System.out.println("Number is not in range with 0 to 1");
+		}
+		
+		
+	
+	}
+
+}
