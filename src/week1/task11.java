c4640aba504f4036fd6a855385b3235f4001657b
TarasNimets <tar.nimets@gmail.com>
2017-09-14 19:41:22 +0300

    added task11

---
+++ src/week1/task11.java
@@ -0,0 +1,22 @@
+package week1;
+
+import java.util.Scanner;
+
+public class task11 {
+	
+	public static void main(String[] arg) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter the number - ");
+		int number = scanner.nextInt();
+		scanner.close();
+		
+		int result = 1;
+		for (int i = number; i > 0; i--) {
+			result *= i; 
+		}
+		System.out.println(result);
+	}
+	
+}
