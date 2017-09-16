cb297aec86a1b1150c9ded7df119ec234207954d
TarasNimets <tar.nimets@gmail.com>
2017-09-16 23:39:58 +0300

    added additional Task6

---
+++ src/week1addition/Task6.java
@@ -0,0 +1,25 @@
+package week1addition;
+
+import java.util.Scanner;
+
+public class Task6 {
+	
+	public static void main(String[] arg) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter the speed in m/s - ");
+		double speed = scanner.nextDouble();
+		
+		System.out.print("Enter the distance in m - ");
+		double distance = scanner.nextDouble();
+		
+		double time = distance / speed * 100.0;
+		int temp = (int) Math.round(time);
+		time = (double) temp / 100;
+		System.out.println("Time of ride = " + time + "s");
+	
+	}
+
+
+}
