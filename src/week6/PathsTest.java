d7c0203f362438394ec40a3c6b26afd48a39aef6
TarasNimets <tar.nimets@gmail.com>
2017-10-20 19:44:06 +0300

    update home work week6

--- src/week6/PathsTest.java
+++ src/week6/PathsTest.java
@@ -5,28 +5,33 @@ import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.regex.Matcher;
 
 public class PathsTest {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
+
+                    // 1 method
         // InputStream inputStream = new URL("http://football.ua/").openStream();
         // Path asd = Paths.get("23");
         // Files.copy(inputStream, asd);
-        // System.out.println(asd);
         // List<String> sdf = Files.readAllLines(asd);
         // System.out.println(sdf);
         //
+        
+                    // 2 method
+        
 //        URL url = new URL("http://football.ua/");
 //        Path path = Paths.get(url.toURI());
 //        List<String> sdf = Files.readAllLines(path);
 //        System.out.println(sdf);
-//        
+
+        
+                    //3 method
+        
         // String urlString = "http://football.ua/";
-        // URI uri = URI.create(urlString);
+//         URI uri = URI.create(urlString);
         // Path path = Paths.get(uri);
         // String sdf = Files.lines(path).reduce("", (l1, l2) -> l1 + "\n" + l2);
         // System.out.println(sdf);
@@ -40,9 +45,9 @@ public class PathsTest {
             url = matcher.group();
             try {
                 if (url.endsWith(".png"))
-                    Files.copy(new URL(url).openStream(), Paths.get(i++ + ".png"));
+                    Files.copy(getInputStream(url), Paths.get(i++ + ".png"));
                 else
-                    Files.copy(new URL(url).openStream(), Paths.get(i++ + ".jpg"));
+                    Files.copy(getInputStream(url), Paths.get(i++ + ".jpg"));
             } catch (IOException e) {
                 e.getMessage();
             }
